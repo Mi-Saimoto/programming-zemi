cd34d5858e04b43be44f62e8bf88fe38ed9557a3
Mi-Saimoto <croism00n@gmail.com>
2021-08-26 07:53:27 +0000

    hello world hyouji

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 以下のコードの意味を改めて理解しよう
+  get 'hello-world' => "foo#hello"
 end
