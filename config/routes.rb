2fefcc44f6ff6fd8885d49ac8d534e200986daf0
Mi-Saimoto <croism00n@gmail.com>
2021-08-26 08:51:39 +0000

    simple tweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tweets
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 以下のコードの意味を改めて理解しよう
